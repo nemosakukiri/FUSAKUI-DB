87dae95c0dcce371b8f12291dd96900fe310ae4c
nemosakukiri <nemosakukiri@gmail.com>
2026-03-23 13:50:36 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// あなたのFirebase設定（以前取得したものです）
 const firebaseConfig = {
-  apiKey: "AIzaSyB5K73evEk33c3VIOlIEkOz2c1IRAiltWM",
+  apiKey: "AIzaSyB5K73evEK33c3VIOlIEkOz2c1IRAiltWM",
   authDomain: "fusakui-db.firebaseapp.com",
   projectId: "fusakui-db",
   storageBucket: "fusakui-db.firebasestorage.app",
   messagingSenderId: "520726255572",
-  appId: "1:520726255572:web:abb29154d5b26e3fdd2841",
-  measurementId: "G-BV3RS60149"
+  appId: "1:520726255572:web:1d3151dfb01c36ccdf3211"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
